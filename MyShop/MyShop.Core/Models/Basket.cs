e5547cea7e898ac2c0790dc598dc0f0265f6d9f2
Md Jamil Uddin <azad1031@gmail.com>
2020-08-11 16:28:56 +0200

    Implement Basket class and Basket Controller

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public Basket(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+        
+    }
+}
---
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,24 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+
+    public class BasketController : Controller
+    {
+
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        // GET: Basket
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
